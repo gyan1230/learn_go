51b70a73d074f7dff045734c6c3bd5f75a10805f
Gyan chand Mohanty <gyanchand.mohanty@gmail.com>
2019-04-12 15:07:16 +0530

    closure and scope

---
+++ 13_function/04_closure/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func wrapper() func() int {
+	x := 10
+	return func() int {
+		x++
+		return x
+	}
+}
+func main() {
+	increment := wrapper()
+	fmt.Println(increment())
+	fmt.Println(increment())
+
+}
