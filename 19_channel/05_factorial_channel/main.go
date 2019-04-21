01d0c8f42f87c61042d90d7d6aa081fec5c0b83f
Gyan chand Mohanty <gyanchand.mohanty@gmail.com>
2019-04-21 19:05:17 +0530

    factorial using channel

---
+++ 19_channel/05_factorial_channel/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	in := gen()
+	f := factorial(in)
+
+	for n := range f {
+		fmt.Println(n)
+	}
+}
+
+func gen() chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			out <- i
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
